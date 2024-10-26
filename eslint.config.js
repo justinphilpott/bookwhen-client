d50c4989e50acb2964ca43a7e3a7c2fff048d017
Justin Philpott <justin@jphil.dev>
2024-10-26 22:43:05 +0100

    chore(config): .nvmrc vite config eslint config - initial [wip]

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+// eslint.config.js
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        browser: true,
+        es6: true,
+        node: true,
+      },
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules, // integrates Prettier rules
+      // Add any custom rules here, e.g.:
+      // "semi": ["error", "always"]
+    },
+  },
+];
