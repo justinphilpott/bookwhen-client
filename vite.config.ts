aa3a9cc88daa44e4f9f247e0081e7bf8aa63419b
Justin Philpott <justin@jphil.dev>
2024-11-11 13:20:12 +0000

    chore(config and helpers): add zod, setup coverage reports, add node types

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,36 @@
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths'; // To support paths from tsconfig
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(), // Automatically resolves TypeScript paths
+    tsconfigPaths(),
   ],
   server: {
-    port: 3000, // Custom server port
-    open: true, // Automatically open the browser on server start
+    port: 3000,
+    open: true,
   },
   test: {
-    globals: true, // Enables global test APIs like 'describe', 'it'
-    environment: 'node', // Change to 'jsdom' for browser-like testing
-    mockReset: true, // Resets mocks between tests
+    globals: true,
+    environment: 'node',
+    mockReset: true,
     coverage: {
-      provider: 'v8', // Use 'c8' for coverage reports
-      reporter: ['text', 'json', 'html'], // Custom coverage report formats
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'dist/**',
+        'src/types/**',
+        'coverage/**',
+        '*.config.*',
+        'services/event/EventInterfaces.ts',
+        'services/event/EventTypes.ts',
+      ]
     },
-    exclude: [...configDefaults.exclude], // Exclude default files (node_modules, etc.)
+    exclude: [...configDefaults.exclude],
   },
   resolve: {
     alias: {
-      '@': '/src', // Simple path aliasing
+      '@': '/src',
     },
   },
 });
