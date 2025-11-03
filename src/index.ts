5642337b856da714cbc1ed7f96e5c5e47655f4fa
Justin Philpott <justin@jphil.dev>
2025-11-03 22:13:31 +0000

    fix(api): fix response structure

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@
 export * from './types/GlobalTypes.js';
 export * from './services/event/EventInterfaces.js';
 export { createBookwhenClient } from './client/BookwhenClient.js';
+export { resolveJsonApiRelationships, resolveJsonApiResource } from './utils/json-api-resolver.js';
